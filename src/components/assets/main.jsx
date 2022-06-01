13f06a0daa6227c14989e9ff5c4b242b25fd27f8
Nikit099 <nechit1337@gmail.com>
2022-06-01 15:23:36 +0300

    made axios

--- src/components/assets/main.jsx
+++ src/components/assets/main.jsx
@@ -1,19 +1,25 @@
 import React, { useState } from 'react'
 import Meangement from '../utils/management'
 import Modal from '../utils/modal'
-
+import axios from 'axios'
 
 function Main() {
     const [isModal, setIsModal] = useState(false)
-    const changeModal = (event) => {
-        event.stopPropagation()
+    const [value, setValue] = useState({ name: '',   company:'', tel: '' , email: '',})
+    const changeModal = () => {
         setIsModal(!isModal)
+        
+        if(value.name && value.company && value.tel && value.email){
+            axios.post('https://61c3f0c4f1af4a0017d99198.mockapi.io/forTeam', value)
+            setValue({name: '',   company:'', tel: '' , email: ''})
+        }
+        
     }
     return (
         <div className='main'>
             <div className="main_container">
             {
-                isModal && <Modal changeModal={changeModal}/>
+                isModal && <Modal changeModal={changeModal} setValue={setValue} value={value}/>
             }
                
                 <img src="https://static.tildacdn.com/tild3134-3339-4562-a266-333832323431/pexels-christina-mor.jpg" alt="" />
--- src/components/utils/modal.jsx
+++ src/components/utils/modal.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 
-function Modal({changeModal}) {
+function Modal({changeModal, value, setValue}) {
   
     return (
         <div className='modal_container'>
@@ -8,10 +8,10 @@ function Modal({changeModal}) {
                    <p className='title'>Для получения доступа укажите</p>
                    <p className='title2'>Контактные данные</p>
                    <div className="contInp">
-                   <input type="name"  placeholder='Имя и фамилия '  />
-                   <input type="text" placeholder='Название организации' />
-                   <input type='tel' placeholder='+7 (999) 999-99-99' />
-                   <input type="email" placeholder='E-mail' />
+                   <input type="name" value={value.name} onChange={(e) => setValue({...value, name: e.target.value})}  placeholder='Имя и фамилия '  />
+                   <input type="text" value={value.company}  onChange={(e) => setValue({...value, company: e.target.value})}  placeholder='Название организации' />
+                   <input type='tel' value={value.tel}  onChange={(e) => setValue({...value, tel: e.target.value})}  placeholder='+7 (999) 999-99-99' />
+                   <input type="email" value={value.email}  onChange={(e) => setValue({...value, email: e.target.value})}  placeholder='E-mail' />
                    </div>
                    <div className="btn2">
                    <button className='get' onClick={changeModal}>Получить доступ</button>
